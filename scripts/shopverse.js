2513e9c93e3442b08460f14285f7417b373b4938
Sneha Das <154408198+Snehadas2005@users.noreply.github.com>
2025-05-20 01:40:20 +0530

    Script

--- scripts/shopverse.js
+++ scripts/shopverse.js
@@ -1,1 +1,721 @@
-console.log("hellos")
+const products = [{
+    image: 'images/products/dress-1.jpg',
+    name: 'Raysaa Corderio Vintage Corset Crop Top with Mini Dobby Skirt',
+    rating: {
+        stars: 4.5,
+        count: 27
+    },
+    priceCount: 1019.00
+}, {
+    image: 'images/products/dress-3.jpg',
+    name: 'BENDISTAR BUTIK White Floral Midi Slit Dress',
+    rating: {
+        stars: 4,
+        count: 22
+    },
+    priceCount: 4893.00
+}, {
+    image: 'images/products/dress-4.jpg',
+    name: 'Uncut High Waist Suspender Retro Slit Tie Dress',
+    rating: {
+        stars: 5,
+        count: 43
+    },
+    priceCount: 2599.00
+}, {
+    image: 'images/products/boots-2.jpg',
+    name: 'Rise Fashion Fuzzy Lace Combat Boots',
+    rating: {
+        stars: 5,
+        count: 45
+    },
+    priceCount: 678.00
+}, {
+    image: 'images/products/boots-1.jpg',
+    name: 'Shoe\'N Tale Stretch Suede Chunky Boots',
+    rating: {
+        stars: 4,
+        count: 87
+    },
+    priceCount: 950.00
+}, {
+    image: 'images/products/boots-3.jpg',
+    name: 'Express Killer White Boots',
+    rating: {
+        stars: 4.5,
+        count: 45
+    },
+    priceCount: 1010.00
+}, {
+    image: 'images/products/boots-4.jpg',
+    name: 'Martin Boots',
+    rating: {
+        stars: 4,
+        count: 32
+    },
+    priceCount: 795.00
+}, {
+    image: 'images/products/boots-6.jpg',
+    name: 'Pubic Desire Burgundy Boots',
+    rating: {
+        stars: 4.5,
+        count: 20
+    },
+    priceCount: 939.00
+}, {
+    image: 'images/products/earrings-1.jpg',
+    name: 'Abaan Gallery Sparkling Crystal Studded Silver Earrings',
+    rating: {
+        stars: 4.5,
+        count: 56
+    },
+    priceCount: 460.00
+}, {
+    image: 'images/products/earrings-2.jpg',
+    name: 'Fida Silver Earrings',
+    rating: {
+        stars: 4,
+        count: 41
+    },
+    priceCount: 479.00
+}, {
+    image: 'images/products/earrings-3.jpg',
+    name: 'Yellow Chimes Silver Tone Elegant Earrings',
+    rating: {
+        stars: 4,
+        count: 16
+    },
+    priceCount: 659.00
+}, {
+    image: 'images/products/earrings-4.jpg',
+    name: 'Abaan Gallery Sparkling Crystal Studded Silver Earrings',
+    rating: {
+        stars: 3,
+        count: 6
+    },
+    priceCount: 567.00
+}, {
+    image: 'images/products/earrings-5.jpg',
+    name: 'Abaan Gallery Sparkling Crystal Studded Silver Earrings',
+    rating: {
+        stars: 3,
+        count: 6
+    },
+    priceCount: 567.00
+}, {
+    image: 'images/products/earrings-3.jpg',
+    name: 'Peave Elongated Pear Drop CZ Earrings',
+    rating: {
+        stars: 4.5,
+        count: 38
+    },
+    priceCount: 3606.00
+}, {
+    image: 'images/products/bag-2.jpg',
+    name: 'Ivy Roses Fairycore Cottagecore Princesscore Bag',
+    rating: {
+        stars: 4,
+        count: 93
+    },
+    priceCount: 1690.00
+}, {
+    image: 'images/products/bag-3.jpg',
+    name: 'Ivy Embroidered Bag',
+    rating: {
+        stars: 3.5,
+        count: 50
+    },
+    priceCount: 830.00
+}, {
+    image: 'images/products/bag-4.jpg',
+    name: 'John Mojie Small Bag with Bow',
+    rating: {
+        stars: 4.5,
+        count: 49
+    },
+    priceCount: 999.00
+}, {
+    image: 'images/products/bag-5.jpg',
+    name: 'Prada Logo-Plaque Mini Bag Black',
+    rating: {
+        stars: 5,
+        count: 102
+    },
+    priceCount: 10800.00
+}, {
+    image: 'images/products/bag-10.jpg',
+    name: 'Palmetto Bucket Peeples Song Palms',
+    rating: {
+        stars: 4.5,
+        count: 46
+    },
+    priceCount: 1645.00
+}, {
+    image: 'images/products/bag-7.jpg',
+    name: 'Pinaken Vegan Leather Satchel Bag',
+    rating: {
+        stars: 5,
+        count: 82
+    },
+    priceCount: 1299.00
+}, {
+    image: 'images/products/combo-6.jpg',
+    name: 'Office Daily Look by Elivn',
+    rating: {
+        stars: 3.5,
+        count: 10
+    },
+    priceCount: 5999.00
+}, {
+    image: 'images/products/combo-2.jpg',
+    name: 'Celadon Print Camisole with Vest',
+    rating: {
+        stars: 4.5,
+        count: 15
+    },
+    priceCount: 5429.00
+}, {
+    image: 'images/products/combo-3.jpg',
+    name: 'Office Look by Aerein',
+    rating: {
+        stars: 4.5,
+        count: 40
+    },
+    priceCount: 9891.00
+}, {
+    image: 'images/products/combo-4.jpg',
+    name: 'Perfect Date night by Jenna Libman',
+    rating: {
+        stars: 5,
+        count: 59
+    },
+    priceCount: 6417.00
+}, {
+    image: 'images/products/combo-5.jpg',
+    name: 'Casual Day look by Jenna Libman',
+    rating: {
+        stars: 3.5,
+        count: 19
+    },
+    priceCount: 4420.00
+}, {
+    image: 'images/products/combo-9.jpg',
+    name: 'Solo Date look by Glitz Africa ',
+    rating: {
+        stars: 5,
+        count: 90
+    },
+    priceCount: 5991.00
+}, {
+    image: 'images/products/combo-10.jpg',
+    name: 'Usual day look by Raysaa Corderio',
+    rating: {
+        stars: 4.5,
+        count: 71
+    },
+    priceCount: 7880.00
+}, {
+    image: 'images/products/heels-1.jpg',
+    name: 'Stuart Heeled Sandal',
+    rating: {
+        stars: 4.5,
+        count: 25
+    },
+    priceCount: 989.00
+}, {
+    image: 'images/products/heels-2.jpg',
+    name: 'STEVE MADDEN Dream-On Chunky-Heeled Sandals',
+    rating: {
+        stars: 4.5,
+        count: 25
+    },
+    priceCount: 989.00
+}, {
+    image: 'images/products/heels-3.jpg',
+    name: 'Penrose Bridal Shoes',
+    rating: {
+        stars: 5,
+        count: 113
+    },
+    priceCount: 1770.00
+}, {
+    image: 'images/products/heels-5.jpg',
+    name: 'MissHeel Lace Up Rhinestone High Heel',
+    rating: {
+        stars: 4.5,
+        count: 67
+    },
+    priceCount: 3414.00
+}, {
+    image: 'images/products/heels-7.jpg',
+    name: 'IRUSSH Classic Black Suedette Ankle Wedges Heels',
+    rating: {
+        stars: 5,
+        count: 78
+    },
+    priceCount: 5000.00
+}, {
+    image: 'images/products/heels-8.jpg',
+    name: 'YOGAYEBow Pointer Toe High Heels',
+    rating: {
+        stars: 4.5,
+        count: 70
+    },
+    priceCount: 1838.00
+}, {
+    image: 'images/products/heels-9.jpg',
+    name: 'Women\'s Pearl Ankle Strap High Heels Closed Toe Stiletto D\'or say',
+    rating: {
+        stars: 4,
+        count: 10
+    },
+    priceCount: 1806.00
+}, {
+    image: 'images/products/heels-10.jpg',
+    name: 'Swish Sandali Sandali Tacco D1-s83 Nero Donna Heels',
+    rating: {
+        stars: 4.5,
+        count: 74
+    },
+    priceCount: 4999.00
+}, {
+    image: 'images/products/jeans-3.jpg',
+    name: 'Masterly Weft Light Blue Color Jeans',
+    rating: {
+        stars: 3.5,
+        count: 32
+    },
+    priceCount: 670.00
+}, {
+    image: 'images/products/jeans-5.jpg',
+    name: 'Gold DA-Fashion\'s Chic Women\'s Jeans ',
+    rating: {
+        stars: 4,
+        count: 27
+    },
+    priceCount: 681.00
+}, {
+    image: 'images/products/jeans-4.jpg',
+    name: 'High Waist Flared Bell-Bottom Denim Jeans',
+    rating: {
+        stars: 4.5,
+        count: 47
+    },
+    priceCount: 789.00
+}, {
+    image: 'images/products/jeans-6.jpg',
+    name: 'Bowknot Straight Leg Jeans For Daily Casual',
+    rating: {
+        stars: 4,
+        count: 27
+    },
+    priceCount: 2392.00
+}, {
+    image: 'images/products/jeans-12.jpg',
+    name: ' Straight Wide Leg Jeans Causal Patchwork Denim Pants with Lace',
+    rating: {
+        stars: 3.5,
+        count: 35
+    },
+    priceCount: 3450.00
+}, {
+    image: 'images/products/jeans-7.jpg',
+    name: 'Ann Springs Boot-Leg Blue Jeans',
+    rating: {
+        stars: 4,
+        count: 60
+    },
+    priceCount: 1896.00
+}, {
+    image: 'images/products/tops-1.jpg',
+    name: 'Green Draped Collar Halter Back Metal Decorative Buckle',
+    rating: {
+        stars: 5,
+        count: 36
+    },
+    priceCount: 1580.00
+}, {
+    image: 'images/products/tops-2.jpg',
+    name: 'Gothic Tie-Hem Top – Goth N\' Rock',
+    rating: {
+        stars: 4.5,
+        count: 39
+    },
+    priceCount: 1275.00
+}, {
+    image: 'images/products/tops-4.jpg',
+    name: 'Floerns Front Sqaure Neck Puff Short Sleeve Tie Back',
+    rating: {
+        stars: 4,
+        count: 45
+    },
+    priceCount: 857.00
+}, {
+    image: 'images/products/tops-5.jpg',
+    name: 'Black Off-The-Shoulder Lace Embellishment Shaper Corset',
+    rating: {
+        stars: 4,
+        count: 28
+    },
+    priceCount: 4151.00
+}, {
+    image: 'images/products/tops-6.jpg',
+    name: 'Floerns Front Sqaure Neck Puff Short Sleeve Tie Back',
+    rating: {
+        stars: 4,
+        count: 45
+    },
+    priceCount: 857.00
+}, {
+    image: 'images/products/tops-8.jpg',
+    name: 'White Peplum TOP with Puff Sleeves ',
+    rating: {
+        stars: 4.5,
+        count: 36
+    },
+    priceCount: 1099.00
+},{
+    image: 'images/products/kurti-black.jpg',
+    name: 'Floerns Front Sqaure Neck Puff Short Sleeve Tie Back',
+    rating: {
+        stars: 4,
+        count: 45
+    },
+    priceCount: 857.00
+}, {
+    image: 'images/products/kurti-blue.jpg',
+    name: 'Georgette Embroidered Stitched Sharara',
+    rating: {
+        stars: 4.5,
+        count: 69
+    },
+    priceCount: 6909.00
+},{
+    image: 'images/products/kurti-dark-pink.jpg',
+    name: 'SHISHKALA Georgette Kurta Dupatta Set',
+    rating: {
+        stars: 3.5,
+        count: 53
+    },
+    priceCount: 984.00
+}, {
+    image: 'images/products/kurti-light-blue.jpg',
+    name: ' Floral Anarkali Kurta Palazzo Dupatta Set',
+    rating: {
+        stars: 3.5,
+        count: 30
+    },
+    priceCount: 1771.00
+}, {
+    image: 'images/products/kurti-light-pink.jpg',
+    name: 'Fancy Satin Kurti Set',
+    rating: {
+        stars: 5,
+        count: 61
+    },
+    priceCount: 2063.00
+},{
+    image: 'images/products/kurti-pink.jpg',
+    name: 'Pink Color Organza Thread Work Anarkali',
+    rating: {
+        stars: 4.5,
+        count: 27
+    },
+    priceCount: 2759.00
+}, {
+    image: 'images/products/kurti-white.jpg',
+    name: 'Dusty Yellow Dupion Silk Sharara Set',
+    rating: {
+        stars: 4,
+        count: 40
+    },
+    priceCount: 1708.00
+},{
+    image: 'images/products/necklase-set-1.jpg',
+    name: 'COCIDE Bride Wedding Jewelry Silver Crystal Set Bridal Rhinestone',
+    rating: {
+        stars: 4,
+        count: 91
+    },
+    priceCount: 2602.00
+}, {
+    image: 'images/products/necklase-set-2.jpg',
+    name: 'Rhinestone Choker And Earrings Set',
+    rating: {
+        stars: 4,
+        count: 31
+    },
+    priceCount: 2138.00
+}, {
+    image: 'images/products/necklase-set-3.jpg',
+    name: 'Rhinestone Decor Jewelry Set',
+    rating: {
+        stars: 3.5,
+        count: 17
+    },
+    priceCount: 2404.00
+}, {
+    image: 'images/products/necklase-set-4.jpg',
+    name: 'Yellow Chimes loral Design Classic CZ Stoned Jewellery Set',
+    rating: {
+        stars: 5,
+        count: 9
+    },
+    priceCount: 1930.00
+},{
+    image: 'images/products/necklase-set-5.jpg',
+    name: 'Cathercing Bridal Crystal Rhinestone Vine',
+    rating: {
+        stars: 4.5,
+        count: 64
+    },
+    priceCount: 5206.00
+}, {
+    image: 'images/products/lehanga-1.jpg',
+    name: 'Yellow Floral Print on Organza Lehenga Choli',
+    rating: {
+        stars: 4,
+        count: 55
+    },
+    priceCount: 2759.00
+},{
+    image: 'images/products/lehanga-2.jpg',
+    name: 'Alankrutha Floral White Pink Lehanga with Hand ',
+    rating: {
+        stars: 4,
+        count: 25
+    },
+    priceCount: 10460.00
+},{
+    image: 'images/products/lehanga-3.jpg',
+    name: 'Pink Georgette Sequence Work Lehenga',
+    rating: {
+        stars: 5,
+        count: 43
+    },
+    priceCount: 1599.00
+},{
+    image: 'images/products/pendant-1.jpg',
+    name: 'Minimalist Sun Necklace, Sun Necklace',
+    rating: {
+        stars: 5,
+        count: 43
+    },
+    priceCount: 1150.00
+},{
+    image: 'images/products/pendant-2.jpg',
+    name: 'Silver Phoenix Necklace, Fire Bird Necklace',
+    rating: {
+        stars: 5,
+        count: 56
+    },
+    priceCount: 2783.00
+},{
+    image: 'images/products/pendant-3.jpg',
+    name: 'Rose Swan Necklace Sterlin',
+    rating: {
+        stars: 3.5,
+        count: 25
+    },
+    priceCount: 670.00
+},{
+    image: 'images/products/pendant-4.jpg',
+    name: 'Shining Stars Charm Sterling Silver Delicate Necklace',
+    rating: {
+        stars: 4,
+        count: 33
+    },
+    priceCount: 1150.00
+},{
+    image: 'images/products/pendant-5.jpg',
+    name: 'Purple Opal Sun Necklace',
+    rating: {
+        stars: 5,
+        count: 99
+    },
+    priceCount: 2049.00
+},{
+    image: 'images/products/pendant-6.jpg',
+    name: 'Shining Diva Heart Shaped Blue Ruby Necklace',
+    rating: {
+        stars: 4.5,
+        count: 31
+    },
+    priceCount: 20500.00
+}, {
+    image: 'images/products/rings-1.jpg',
+    name: 'SWEETPEEPS Lined Up Hearts Gold Plated',
+    rating: {
+        stars: 5,
+        count: 16
+    },
+    priceCount: 1783.00
+}, {
+    image: 'images/products/rings-4.jpg',
+    name: 'VSAP Rose Gold Red Rose Flower Ring',
+    rating: {
+        stars: 4.5,
+        count: 89
+    },
+    priceCount: 2817.00
+},{
+    image: 'images/products/rings-5.jpg',
+    name: 'ILURRALDE White Sapphire Crown Rose Gold',
+    rating: {
+        stars: 3.5,
+        count: 58
+    },
+    priceCount: 3476.00
+}, {
+    image: 'images/products/rings-6.jpg',
+    name: 'Enchanted Rose Ring',
+    rating: {
+        stars: 4,
+        count: 50
+    },
+    priceCount: 1887.00
+}, {
+    image: 'images/products/rings-7.jpg',
+    name: 'GALAXY Butterfly Pair Ring ',
+    rating: {
+        stars: 5,
+        count: 120
+    },
+    priceCount: 3580.00
+}, {
+    image: 'images/products/rings-6.jpg',
+    name: 'ASTRAY Leafy Ring',
+    rating: {
+        stars: 4,
+        count: 62
+    },
+    priceCount: 2997.00
+},{
+    image: 'images/products/rings-10.jpg',
+    name: 'BlueStone The Jasiel Ring',
+    rating: {
+        stars: 4.5,
+        count: 81
+    },
+    priceCount: 67482.00
+},{
+    image: 'images/products/saree-black.jpg',
+    name: 'Handloom Tussar saree in Grey color',
+    rating: {
+        stars: 4.5,
+        count: 12
+    },
+    priceCount: 6300.00
+},{
+    image: 'images/products/sare-peach.jpg',
+    name: 'Kalista Sequinned Organza Saree',
+    rating: {
+        stars: 4,
+        count: 17
+    },
+    priceCount: 7440.00
+},{
+    image: 'images/products/sare-white.jpg',
+    name: 'Miranda House White Embroidered Georgette Saree',
+    rating: {
+        stars: 5,
+        count: 72
+    },
+    priceCount: 5500.00
+}, {
+    image: 'images/products/skirts-1.jpg',
+    name: 'Berrylush Black Floral Printed A-line Side Slit Maxi Skirt',
+    rating: {
+        stars: 4,
+        count: 45
+    },
+    priceCount: 556.00
+}, {
+    image: 'images/products/skirts-2.jpg',
+    name: 'Y&D Denim Mini Skirt',
+    rating: {
+        stars: 3.5,
+        count: 76
+    },
+    priceCount: 509.00
+},{
+    image: 'images/products/skirts-3.jpg',
+    name: 'Galz Corner Light Blue A-line Denim Skirt',
+    rating: {
+        stars: 5,
+        count: 82
+    },
+    priceCount: 699.00
+}, {
+    image: 'images/products/skirts-4.jpg',
+    name: 'Galz Corner Blue A-line Denim Skirt',
+    rating: {
+        stars: 4.5,
+        count: 76
+    },
+    priceCount: 648.00
+}, {
+    image: 'images/products/skirts-5.jpg',
+    name: 'Tokyo Talkies Beige A-line Skirt',
+    rating: {
+        stars: 5,
+        count: 98
+    },
+    priceCount: 719.00
+}, {
+    image: 'images/products/skirts-6.jpg',
+    name: 'Preppy Style Summer Mini Skirt',
+    rating: {
+        stars: 4,
+        count: 63
+    },
+    priceCount: 828.00
+},{
+    image: 'images/products/skirts-7.jpg',
+    name: 'AISWI Satin High Waist Midi Silk Swing Skirt',
+    rating: {
+        stars: 5,
+        count: 8
+    },
+    priceCount: 6840.00
+}, {
+    image: 'images/products/skirts-10.jpg',
+    name: 'Mast & Harbour Red & White Floral Print Midi Skirt',
+    rating: {
+        stars: 4.5,
+        count: 69
+    },
+    priceCount: 999.00
+},{
+    image: 'images/products/sneakers-1.jpg',
+    name: 'Vulcanize Black White Platform Thick Sole Running Casual Shoe ',
+    rating: {
+        stars: 4.5,
+        count: 40
+    },
+    priceCount: 3329.00
+},{
+    image: 'images/products/sneakers-3.jpg',
+    name: 'Black Pink Adidas Galaxy 6 Shoes ',
+    rating: {
+        stars: 4.5,
+        count: 40
+    },
+    priceCount: 4199.00
+},{
+    image: 'images/products/sneakers-5.jpg',
+    name: 'FLAGELI White Leather Platform Sneakers',
+    rating: {
+        stars: 4,
+        count: 76
+    },
+    priceCount: 1999.00
+},{
+    image: 'images/products/sneakers-4.jpg',
+    name: 'Cruiser Chunky Sneaker for Bold & Stylish Women',
+    rating: {
+        stars: 4,
+        count: 214
+    },
+    priceCount: 999.00
+},];
