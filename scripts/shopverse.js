191e191added5300f1fe2a6033c2e5b531bec62f
Sneha Das <154408198+Snehadas2005@users.noreply.github.com>
2025-05-21 01:21:12 +0530

    change7

--- scripts/shopverse.js
+++ scripts/shopverse.js
@@ -1,5 +1,6 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import { products } from '../data/products.js';
+
 
 let productsHTML = '';
 
@@ -52,51 +53,31 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+function updateCartQuantity() {
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
+let addedMessageTimeoutId;
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-    let addedMessageTimeoutId;
-    
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
         
-        let matchingItem;
-
-        cart.forEach((item) => {
-            if (productId === item.productId) {
-                matchingItem = item;
-            }
-        });
-
-        const quantitySelector = document.querySelector(
-            `.js-quantity-selector-${productId}`
-        );
-
-        const quantity = Number(quantitySelector.value);
-
-        if (matchingItem) {
-            matchingItem.quantity += quantity;
-        } else {
-            cart.push({
-                productId,
-                quantity
-            });
-        }
-
-        let cartQuantity = 0;
-
-        cart.forEach((item) => {
-            cartQuantity += item.quantity;
-        });
-
-        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-      
+        const quantity = addToCart(productId);
+        
+        updateCartQuantity();
+        
         const addedMessage = document.querySelector(
             `.js-added-to-cart-${productId}`
         );
         
         if (addedMessage) {
-            
-            addedMessage.offsetWidth;
-            
             addedMessage.classList.add('added-to-cart-visible');
 
             if (addedMessageTimeoutId) {
