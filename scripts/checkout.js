5e853ae13caa97f67f35331bf7a60c59b0fcf88b
Sneha Das <154408198+Snehadas2005@users.noreply.github.com>
2025-06-04 00:06:19 +0530

    checkout header is added and modifications are made

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,9 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { cart } from "../data/cart.js";
+import { renderCheckoutHeader } from "./checkout/checkoutheader.js";
+
+renderCheckoutHeader();
 
 function renderEmptyCart() {
   const mainElement = document.querySelector('.main');
---
+++ scripts/checkout/checkoutheader.js
@@ -0,0 +1,35 @@
+import { cart } from "../../data/cart.js";
+
+export function renderCheckoutHeader() {
+    let cartQuantity = 0;
+
+    cart.forEach((carItem) => {
+        cartQuantity += carItem.quantity;
+    });
+
+    const checkoutHeaderHTML = 
+    `
+        <div class="checkout-header">
+            <div class="header-content">
+                <div class="checkout-hhttps://www.learncbse.in/ncert-solutions-for-class-8-english-honeydew-bepin-choudhurys-lapse-of-memory/#google_vignetteeader-left-section">
+                <a href="shopverse.html">
+                    <img class="shopverse-logo" src="images/shopverse-black.png">
+                    <img class="shopverse-mobile-logo" src="images/logo-black.png">
+                </a>
+                </div>
+
+                <div class="checkout-header-middle-section">
+                <a class="return-to-home-link js-return-to-home-link"
+                    href="shopverse.html"></a>
+                </div>
+
+                <div class="checkout-header-right-section">
+                    <img src="images/icons/checkout-lock-icon.png">
+                </div>
+            </div>
+        </div>  
+    `
+
+    document.querySelector('.js-checkout-header')
+        .innerHTML = checkoutHeaderHTML;
+}
